97ce102c597f37bad558a3a74b443842504434e6
barto14753 <56938330+barto14753@users.noreply.github.com>
2021-07-09 22:24:54 +0200

    .

--- SocialNetwork/home/views.py
+++ SocialNetwork/home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Home</h1>")
+    context = {
+        "title": "Social Network"
+    }
+    return render(request, "index.html", context=context)
