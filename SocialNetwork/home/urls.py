fe6086fd4709fddd2e5f6df9185eb03a97a3b6eb
barto14753 <56938330+barto14753@users.noreply.github.com>
2021-07-19 00:08:48 +0200

    some css of sign forms

--- SocialNetwork/home/urls.py
+++ SocialNetwork/home/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signUp/', views.signIn, name='signIn'),
-    path('signIn/', views.signUp, name='signUp'),
+    path('signUp/', views.signUp, name='signUp'),
+    path('signIn/', views.signIn, name='signIn'),
 ]
